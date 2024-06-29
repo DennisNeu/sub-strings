1fd29e01531882c4ef03b6dd1d07464566a104fc
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-06-29 21:29:19 +0200

    initial commit

---
+++ script.rb
@@ -0,0 +1,8 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dict)
+  if string.length == 0
+    puts "Error: String length 0"
+    return
+  end
+end
