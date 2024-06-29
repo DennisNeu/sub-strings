fc84c7d32cee0f0fb7a03df2943fee0c8b2d746c
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-06-29 21:55:48 +0200

    Add search for substring

--- script.rb
+++ script.rb
@@ -5,4 +5,20 @@ def substrings(string, dict)
     puts "Error: String length 0"
     return
   end
+
+  cleaned_string = string.downcase.gsub(/[^a-zA-Z ]/, '')
+  string_array = cleaned_string.split(" ")
+
+  puts string_array
+
+  dict.reduce(Hash.new(0)) do |hash, value|
+    string_array.each do |substring|
+      if substring.include?(value)
+        hash[value] += 1
+      end
+    end
+    hash
+  end
 end
+
+puts substrings("Howdy partner, sit down! How's it going", dictionary)
